023deb804fce9602f7e64cc165d29ac871cdf2ae
ziyaadfredericks <zfredericks00@gmail.com>
2021-04-20 12:17:22 +0200

    Added modified Newton's Method.

--- COS2633/Chapter 2 - Solutions of Equations in One Variable/modified_newtons_method.py
+++ COS2633/Chapter 2 - Solutions of Equations in One Variable/modified_newtons_method.py
@@ -1,15 +1,18 @@
-# Finds the root of an equation in the range [a, b] by using Newton's Method.
+# Finds the root of an equation in the range [a, b] by using the modified version of Newton's Method.
 
 import math
 
 def equation(x):
 	return pow(x, 2) + 4 * x #Original equation.
 
-def derivative(x):
+def derivative1(x):
 	return 2 * x + 4 #Derivative of original equation.
 
+def derivative2(x):
+	return 2 #2nd Derivative of original equation.
+
 def newton(p0, i):
-	p = p0 - (equation(p0) / derivative(p0))
+	p = p0 - ((equation(p0) * derivative1(p0)) / (pow(derivative1(p0), 2) - (equation(p0) * derivative2(p0))))
 	print("\n(p" + str(i) + ", f(p" + str(i) + ")) = (" + str((p, equation(p))) + ")")
 
 	if(equation(p) != 0):
