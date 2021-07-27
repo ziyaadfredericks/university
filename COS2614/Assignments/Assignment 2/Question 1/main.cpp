f689aea6bb5c11f3790da02ec388011740500d59
Ziyaad Fredericks <zfredericks00@gmail.com>
2021-07-27 13:08:45 +0200

    Update main.cpp

--- COS2614/Assignments/Assignment 2/Question 1/main.cpp
+++ COS2614/Assignments/Assignment 2/Question 1/main.cpp
@@ -18,20 +18,6 @@ int main(int argc, char *argv[]) {
 		products.push_back(prod);
 	}
 
-	for (int i = 0; i < 3; ++i) {
-		Product prod(QString::number(i + 1), i + 1, (i + 1) * 3, (i + 1) * 17.67);
-		products.push_back(prod);
-	}
-
-	for (int i = 0; i < 3; ++i) {
-		Product prod(QString::number(i + 1), i + 1, (i + 1) * 3, (i + 1) * 17.67);
-		products.push_back(prod);
-	}
-
-	for (auto &product : products) {
-		productList.add(&product);
-	}
-
 	productList.printAll();
 
 	return QCoreApplication::exec();
